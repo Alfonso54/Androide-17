e3857dedbaf4f6324314966c97e7b11cc3879b89
Daspssj <diegosierraaaaa@gmail.com>
2022-11-12 21:48:44 -0300

    a

--- AVANCE_SEBA/blog/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms 
-from .models import BlogsPost
-
-class AutoForm(forms.ModelForm):
-    class meta:
-        model = BlogsPost
-        exclude = ['timestamp']
--- AVANCE_SEBA/blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, path
-from blog import views
-
-urlpatterns = [
-    path('homepageindex/',views.homepagecolor,name='homepageindex'),
-]
