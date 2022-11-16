b0a1d529d9939638aff6f3b81fc45cf068d49e2f
Daspssj <diegosierraaaaa@gmail.com>
2022-11-15 22:41:46 -0300

    modificado 1

--- Proyecto/A_17_app/models.py
+++ Proyecto/A_17_app/models.py
@@ -6,4 +6,5 @@ class Auto(models.Model):
 
     modelo = models.CharField(max_length=100)
     marca = models.CharField(max_length=100)
+    patente = models.CharField(max_length=100, blank=True)
     fecha = models.DateTimeField(auto_now=True)
